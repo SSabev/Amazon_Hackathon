6eeaf7da1cf4ac0c6c406579adfacaa0399b076c
YordanPetrov <yordanpetrow@gmail.com>
2013-04-06 05:24:10 +0100

    change in app design

--- amazon_hack/gen/com/example/amazon_hack/R.java
+++ amazon_hack/gen/com/example/amazon_hack/R.java
@@ -39,11 +39,12 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int connect=0x7f020001;
         public static final int curly=0x7f020002;
         public static final int ic_launcher=0x7f020003;
-        public static final int mic=0x7f020004;
+        public static final int icon=0x7f020004;
+        public static final int mic=0x7f020005;
     }
     public static final class id {
-        public static final int action_settings=0x7f0a000b;
-        public static final int btSpeak=0x7f0a000a;
+        public static final int action_settings=0x7f0a000d;
+        public static final int btSpeak=0x7f0a000c;
         public static final int connect_message=0x7f0a0001;
         public static final int curly_bottom=0x7f0a0007;
         public static final int enter_code=0x7f0a0002;
@@ -52,8 +53,10 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int imageView2=0x7f0a0006;
         public static final int ok_button=0x7f0a0003;
         public static final int or_message=0x7f0a0004;
+        public static final int or_message2=0x7f0a000a;
         public static final int scan_qr_message=0x7f0a0005;
         public static final int submit_button=0x7f0a0009;
+        public static final int type_message=0x7f0a000b;
     }
     public static final class layout {
         public static final int activity_homescreen=0x7f030000;
@@ -65,23 +68,24 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int send_code=0x7f090001;
     }
     public static final class string {
-        public static final int action_settings=0x7f060008;
+        public static final int action_settings=0x7f060009;
         public static final int app_name=0x7f060000;
-        public static final int btSpeak=0x7f06000a;
-        public static final int button_OK=0x7f060006;
+        public static final int btSpeak=0x7f06000b;
+        public static final int button_OK=0x7f060007;
         public static final int code_hint=0x7f060004;
         public static final int connect_message=0x7f060003;
         public static final int content_hint=0x7f060005;
-        public static final int etSearchHint=0x7f06000f;
-        public static final int hello_world=0x7f060009;
-        public static final int menu_settings=0x7f06000b;
+        public static final int etSearchHint=0x7f060010;
+        public static final int hello_world=0x7f06000a;
+        public static final int menu_settings=0x7f06000c;
         public static final int or_message=0x7f060001;
-        public static final int sNoOfMatches=0x7f06000e;
+        public static final int sNoOfMatches=0x7f06000f;
         public static final int scan_qr_message=0x7f060002;
-        public static final int title_activity_search_music=0x7f060010;
-        public static final int title_activity_send_code=0x7f060007;
-        public static final int title_activity_voice_recognition=0x7f06000c;
-        public static final int tvTextMatches=0x7f06000d;
+        public static final int title_activity_search_music=0x7f060011;
+        public static final int title_activity_send_code=0x7f060008;
+        public static final int title_activity_voice_recognition=0x7f06000d;
+        public static final int tvTextMatches=0x7f06000e;
+        public static final int type_message=0x7f060006;
     }
     public static final class style {
         /** 
